7d9b6192202228fbdabc6ed25a8545d84a3cd6cb
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:09:16 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void f(long long m1[2][2], long long m2[2][2]){
+
+    long long tmp[2][2];
+    long long temp;
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            temp = 0;
+            for(int k=0;k<2;k++){
+                temp += (m1[i][k]*m2[k][j]);
+            }
+            tmp[i][j] = temp%1000000007;
+        }
+    }
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            m1[i][j] = tmp[i][j];
+        }
+    }
+
+    return;
+}
+
+int main() {
+    long long mat[2][2] = {1, 1, 1, 0};
+    long long res[2][2] = {1, 0, 0, 1};
+    long long n;
+
+    scanf("%lld",&n);
+    while(n>0){
+        if(n%2 == 1){
+            f(res, mat);
+        }
+
+        n /= 2;
+        f(mat, mat);
+    }
+
+    printf("%lld",res[1][0]);
+}
