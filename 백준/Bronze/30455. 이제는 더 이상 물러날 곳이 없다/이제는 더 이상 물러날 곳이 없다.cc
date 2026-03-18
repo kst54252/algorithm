e26cdc5f365f61781abae08381bca646b14c27d4
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:03:40 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+char s[1005];
+int main(){
+    cin >> n;
+    if(n%2==0)
+        cout<<"Duck";
+    else
+        cout<<"Goose";
+    return 0;
+}
