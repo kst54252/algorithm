323d193feb43ff5f71004b2218fdace3c5639cb8
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:00:47 +0900

    [Silver V] Title: 숫자 카드, Time: 436 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<int,int> m;
+int n,nn,a;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a;
+        m.insert({a,1});
+    }
+    cin >> nn;
+    for(int i=0;i<nn;i++){
+        cin >> a;
+        if(m.find(a)!=m.end())
+            cout<<"1 ";
+        else
+            cout<<"0 ";
+    }
+    return 0;
+}
