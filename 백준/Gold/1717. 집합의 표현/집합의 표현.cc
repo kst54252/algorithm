82d1788dfa9fac90929014222ebd895fbf3ff04e
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:13:07 +0900

    [Gold V] Title: 집합의 표현, Time: 48 ms, Memory: 6324 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, a, b, c, parent[1000001];
+
+int get_parent(int x){
+    if(parent[x]==x)
+        return x;
+    return parent[x]=get_parent(parent[x]);
+}
+
+void union_parent(int a, int b){
+    a=get_parent(a);
+    b=get_parent(b);
+    if(a>b)
+        parent[a]=b;
+    else
+        parent[b]=a;
+
+}
+void find_parent(int a, int b){
+    a=get_parent(a);
+    b=get_parent(b);
+    if(a==b)
+        printf("YES\n");
+    else
+        printf("NO\n");
+}
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+
+    scanf("%d %d",&n, &m);
+    for(int i=1;i<=n;i++)
+        parent[i]=i;
+    for(int i=0;i<m;i++){
+        scanf("%d %d %d", &a, &b, &c);
+        if(!a){
+            union_parent(b,c);
+        }
+        else{
+            find_parent(b,c);
+
+        }
+    }
+    return 0;
+}
