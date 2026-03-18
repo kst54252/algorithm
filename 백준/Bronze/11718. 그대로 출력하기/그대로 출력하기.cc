c50f65ab8da8c8b34e9adc112de83240ed10ebf7
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:06:11 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string s;
+int main()
+{
+    while(1){
+        getline(cin,s);
+        if(s=="")
+            break;
+        cout << s<<"\n";
+    }
+    return 0;
+}
