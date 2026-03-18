b959797eaa4d3aa5b468258be338ad44c116f0a9
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:05:15 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 808 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int n,d[2501];
+char s[2502];
+
+bool f(int l, int r){
+    int ch = 0;
+    for (int i=l; i<=(l+r)/2; i++)
+        if(s[i] != s[r+l-i]){
+            ch = 1;
+        }
+    return ch;
+}
+
+int main(){
+    scanf("%s",&s);
+    n = strlen(s);
+    for (int i=1;i<=n;i++){
+        d[i]=d[i-1]+1;
+        for (int j=i-1;j>=1;j--)
+            if (!f(j-1,i-1)){
+                d[i]=min(d[i],d[j-1]+1);
+            }
+        //printf("%d ",d[i]);
+    }
+    printf("%d",d[n]);
+    return 0;
+}
