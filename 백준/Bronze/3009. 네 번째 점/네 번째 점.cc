40bfdf3571e437bbe2e702b860380a2c29316be3
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:02:12 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, m,x[1001],y[1001];
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    for(int i=0;i<3;i++){
+    cin >> n >> m;
+    x[n]++;
+    y[m]++;
+    }
+    for(int i=0;i<1001;i++)
+        if(x[i]==1)
+        cout<<i<<" ";
+    for(int i=0;i<1001;i++)
+        if(y[i]==1)
+            cout<<i;
+    return 0;
+}
