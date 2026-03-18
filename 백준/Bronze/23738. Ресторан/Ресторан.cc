c13127f31a0b9e4149a716fc67db8079bf841e44
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:43:35 +0900

    [Bronze II] Title: Ресторан, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/23738. Ресторан/Ресторан.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int main() {
+    cin >> s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='B')
+            cout<<"v";
+        if(s[i]=='E')
+            cout<<"ye";
+        if(s[i]=='H')
+            cout<<"n";
+        if(s[i]=='P')
+            cout<<"r";
+        if(s[i]=='C')
+            cout<<"s";
+        if(s[i]=='Y')
+            cout<<"u";
+        if(s[i]=='X')
+            cout<<"h";
+        if(s[i]=='A')
+            cout<<"a";
+        if(s[i]=='K')
+            cout<<"k";
+        if(s[i]=='M')
+            cout<<"m";
+        if(s[i]=='O')
+            cout<<"o";
+        if(s[i]=='T')
+            cout<<"t";
+    }
+
+    return 0;
+}
