b2ba59796e4854f5cbe1c4146ddecaa9c35a9d47
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:03:32 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n >> m;
+    for(int i=0;i<m-1;i++)
+        cout<<1;
+    cout<<n;
+    return 0;
+}
