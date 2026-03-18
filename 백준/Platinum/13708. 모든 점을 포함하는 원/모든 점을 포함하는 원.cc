82a358537da8eb959f5e9a579566b3cc14bce706
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:46:10 +0900

    [Platinum I] Title: 모든 점을 포함하는 원, Time: 64 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Platinum/13708. 모든 점을 포함하는 원/모든 점을 포함하는 원.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+double a[301], b[301], x, y, r = 0.1, ans, cur;
+int n, mm;
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> a[i] >> b[i];
+        x += a[i];
+        y += b[i];
+    }
+    x /= n;
+    y /= n;
+
+    for (int i=0;i<100000;i++){
+        mm = 0;
+        ans = 0;
+        for(int j=0;j<n;j++){
+            cur = sqrt((x - a[j]) * (x - a[j]) + (y - b[j]) * (y - b[j]));
+            if(ans < cur){
+                ans = cur;
+                mm = j;
+            }
+        }
+        x += (a[mm] - x) * r;
+        y += (b[mm] - y) * r;
+        r *= 0.999;
+    }
+    ans *= 2;
+    cout << fixed;
+    cout.precision(2);
+    cout << ans;
+
+    return 0;
+}
