387cfa1295145598fd34992347d203759d78d5ad
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:57:38 +0900

    [Silver II] Title: Pipelined, Time: 32 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/30874. Pipelined/Pipelined.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a[200001];
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    sort(a,a+n);
+    cout<<a[n-1]+n-1;
+}
