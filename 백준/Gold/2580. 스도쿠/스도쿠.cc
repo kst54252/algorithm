d0dfa5f90204c32b090974bd0379072891c90faf
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:56:04 +0900

    [Gold IV] Title: 스도쿠, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int a[9][9],xxx;
-bool row[9][10], col[9][10], rec[3][3][10];
+bool row[10][10], col[10][10], rec[3][3][10];
 vector<pair<int,int>> v;
 void f(int n){
     if(!xxx){
@@ -19,14 +19,14 @@ void f(int n){
         int y = v[n].second;
         for(int i=1;i<=9;i++)
             if(!row[x][i] && !col[y][i] && !rec[x/3][y/3][i]){
-                row[x][i] = 1;
-                col[y][i] = 1;
-                rec[x/3][y/3][i] = 1;
-                a[x][y] = i;
+                row[x][i]=1;
+                col[y][i]=1;
+                rec[x/3][y/3][i]=1;
+                a[x][y]=i;
                 f(n+1);
-                row[x][i] = 0;
-                col[y][i] = 0;
-                rec[x/3][y/3][i] = 0;
+                row[x][i]=0;
+                col[y][i]=0;
+                rec[x/3][y/3][i]=0;
                 a[x][y]=0;
             }
     }
