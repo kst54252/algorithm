fb40875e77982546a8c1f92cafdf349f69ddb1b8
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:07:54 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n,tot,a,b;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> tot;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a >> b;
+        tot-=a*b;
+    }
+    if(!tot)
+        cout<<"Yes";
+    else
+        cout<<"No";
+    return 0;
+}
