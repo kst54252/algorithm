420d45d2e5749a78cd1083ec1ac12b9d325e057e
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:46:16 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a,b;
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    cin >> a >> b;
+    cout<<min(a,b)*50;
+
+    return 0;
+}
