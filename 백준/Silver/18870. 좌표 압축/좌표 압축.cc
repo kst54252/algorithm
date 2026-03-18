284ee973ee2c25950ffaef41c6a54b203c320a35
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:10:37 +0900

    [Silver II] Title: 좌표 압축, Time: 1016 ms, Memory: 22648 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+long long a;
+vector<long long> x1;
+vector<long long> x2;
+
+int main(){
+    
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> a;
+        x1.push_back(a); 
+        x2.push_back(a); 
+    } 
+    
+    sort(x1.begin(), x1.end());
+    x1.erase(unique(x1.begin(), x1.end()), x1.end());
+ 
+    for (int i = 0; i < n; i++)
+        cout << lower_bound(x1.begin(), x1.end(), x2[i]) - x1.begin() << " "; 
+
+}
