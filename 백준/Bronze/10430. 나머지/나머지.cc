9c95d900c45ef86effff92bada796752740a3a26
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:15:23 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int A,B,C;
+int main()
+{
+    scanf("%d %d %d",&A,&B,&C);
+    printf("%d\n",(A+B)%C);
+    printf("%d\n",((A%C)+(B%C))%C);
+    printf("%d\n",(A*B)%C);
+    printf("%d\n",((A%C)*(B%C))%C);
+    return 0;
+}
