cdfe76482917965d0675db3db63023e01e0ba954
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:43:29 +0900

    [Silver V] Title: 새치기하지 마!!!, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/33622. 새치기하지 마！！！/새치기하지 마！！！.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    cout << n <<"\n";
+    for(int i=0;i<n;i++)
+        cout<<"1 ";
+    return 0;
+}
