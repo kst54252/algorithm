f98091b16866b86ce13ba77864678a8c63f56dc5
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:56:23 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n,d[1000001]={0,1};
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+            if(i+1<=1000000)
+                if(d[i+1]>d[i]+1||!d[i+1])
+                    d[i+1]=d[i]+1;
+            if(i*2<=1000000)
+                if(d[i*2]>d[i]+1||!d[i*2])
+                    d[i*2]=d[i]+1;
+            if(i*3<=1000000)
+                if(d[i*3]>d[i]+1||!d[i*3])
+                    d[i*3]=d[i]+1;
+        }
+    printf("%d",d[n]-1);
+    return 0;
+}
