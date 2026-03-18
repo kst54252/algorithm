615c8b9d768135daa371333edbd02b9a0e6c359a
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:42:06 +0900

    [Platinum II] Title: 고속도로, Time: 648 ms, Memory: 7088 KB -BaekjoonHub

---
+++ 백준/Platinum/10254. 고속도로/고속도로.cc
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+struct POINT{
+    ll x, y;
+    int x_d, y_d;
+};
+
+bool com(POINT &p, POINT &q){
+    if(1LL * p.y_d * q.x_d != 1LL * p.x_d * q.y_d)
+        return  1LL * p.y_d * q.x_d < 1LL * p.x_d * q.y_d;
+    if(p.y != q.y)
+        return p.y < q.y;
+    return p.x < q.x;
+}
+bool ccw(const POINT &A, const POINT &B, const POINT &C){
+    return (1LL * (A.x * B.y + B.x * C.y + C.x * A.y - B.x * A.y - C.x * B.y - A.x * C.y)) > 0;
+}
+POINT arr[200002];
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int nn;
+    cin >> nn;
+
+for(int k=0;k<nn;k++){
+    memset(arr, 0, 200002);
+    
+    ll a, b, n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a >> b;
+        arr[i].x = a;
+        arr[i].y = b;
+    }
+    
+    sort(arr, arr+n, com);
+
+    for(int i=1;i<n;i++){
+        arr[i].x_d = arr[i].x - arr[0].x;
+        arr[i].y_d = arr[i].y - arr[0].y;
+    }
+    sort(arr+1, arr+n, com);
+    
+    vector<int> s;
+
+    s.push_back(0);
+    s.push_back(1);
+    ll cnt = 2, j=1;
+    while(cnt < n){
+        while(s.size() >= 2){
+            ll first, second;
+            second = s[s.size()-1];
+            s.pop_back();
+            first = s[s.size()-1];
+
+            if(ccw(arr[first], arr[second], arr[cnt])){
+                s.push_back(second);
+                break;
+            }
+        }
+        s.push_back(cnt++);
+    }
+    if(s.size() == 2)
+        cout << arr[s[0]].x << " " << arr[s[0]].y << " " << arr[s[1]].x << " " << arr[s[1]].y <<"\n";
+    else{
+        ll ans = 0, a1, a2, a3, a4;
+        for(int i=0;i<s.size();i++){
+            int n_i = (i+1) % s.size();
+            while(1){
+                ll n_j = (j+1) % s.size();
+                POINT v1, v2;
+                v1.x = arr[s[n_i]].x - arr[s[i]].x;
+                v1.y = arr[s[n_i]].y - arr[s[i]].y;
+
+                v2.x = arr[s[n_j]].x - arr[s[j]].x;
+                v2.y = arr[s[n_j]].y - arr[s[j]].y;
+
+                ll cross = v1.x * v2.y - v1.y * v2.x;
+                if(cross > 0)
+                    j = n_j;
+                else
+                    break;
+            }
+            if(ans < (arr[s[i]].x-arr[s[j]].x)*(arr[s[i]].x-arr[s[j]].x)+(arr[s[i]].y-arr[s[j]].y)*(arr[s[i]].y-arr[s[j]].y)){
+                ans = (arr[s[i]].x-arr[s[j]].x)*(arr[s[i]].x-arr[s[j]].x)+(arr[s[i]].y-arr[s[j]].y)*(arr[s[i]].y-arr[s[j]].y);
+                a1 = arr[s[i]].x;
+                a2 = arr[s[i]].y;
+                a3 = arr[s[j]].x;
+                a4 = arr[s[j]].y;
+            }
+        }
+        cout << a1 << " " << a2 << " " << a3 << " " << a4 <<"\n";
+}
+}
+    return 0;
+}
