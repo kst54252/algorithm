92d378d59195376825dad29c91c5d13883377cba
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:03:29 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b=1;
+    while(1){
+    cin >> a >> b;
+        if(a==0&&b==0)
+            break;
+        if(a>b)
+            cout<<"Yes\n";
+        else
+            cout<<"No\n";
+    }
+    return 0;
+}
