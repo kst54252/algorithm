724b454fd24a98d3bf099879d25fec55201669c2
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:41:40 +0900

    [Bronze I] Title: 원의 분할, Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/16478. 원의 분할/원의 분할.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed << setprecision(10) << a*c/b;
+    return 0;
+}
