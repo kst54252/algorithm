a2f8e77b0aca46151cd58d4e2154b7005e86308d
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:02:00 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[3];
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    while(1){
+        cin >> a[0] >>a[1]>>a[2];
+        sort(a,a+3);
+        if(a[0]==0)
+            break;
+        if(a[2]>=a[0]+a[1])
+            cout<<"Invalid\n";
+        else if(a[0]==a[1]&&a[0]==a[2]&&a[1]==a[2])
+            cout<<"Equilateral\n";
+        else if(a[0]==a[1]||a[0]==a[2]||a[1]==a[2])
+            cout<<"Isosceles\n";
+        else
+            cout<<"Scalene\n";
+    }
+    return 0;
+}
