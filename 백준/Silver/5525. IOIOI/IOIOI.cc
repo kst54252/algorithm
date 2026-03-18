6d463f71fab585250dc86e4d1893c951c5ca128d
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:10:31 +0900

    [Silver I] Title: IOIOI, Time: 52 ms, Memory: 8824 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, m, ans,a[1000001];
+string s;
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    cin >> n;
+    cin >> m;
+    cin >> s;
+    for(int i=2;i<m;i++)
+        if(s[i-2]=='I'&&s[i-1]=='O'&&s[i]=='I')
+            a[i]=a[i-2]+1;
+    for(int i=2;i<m;i++)
+        if(a[i]>=n)
+            ans++;
+    cout<<ans;
+    return 0;
+}
