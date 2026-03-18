ffe31f22e32b101cbcd2cab8bcef445f17fb75d5
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 02:06:00 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n-i;j++)
+            cout<<" ";
+        for(int j=0;j<i*2-1;j++)
+            cout<<"*";
+        cout<<"\n";
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n-i;j++)
+            cout<<" ";
+        for(int j=0;j<i*2-1;j++)
+            cout<<"*";
+        cout<<"\n";
+    }
+    return 0;
+}
