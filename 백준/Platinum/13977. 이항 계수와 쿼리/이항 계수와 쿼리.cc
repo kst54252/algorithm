e6b19821cdb7cc6333b3d12c3100e89bd972c18c
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-19 01:53:26 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 112 ms, Memory: 33272 KB -BaekjoonHub

---
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9+7;
+ll t, n, k, d[4000001]={1,1};
+ll f(ll a, ll b){
+    if(b==1)return a%mod;
+    if(b%2)return f(a,b-1)*a%mod;
+    return f(a*a%mod, b/2);
+}
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> t;
+    for(int i=2;i<=4000000;i++)
+        d[i]=d[i-1]*i%mod;
+    for(int i=0;i<t;i++){
+        cin >> n >> k;   
+        cout << d[n] *f(d[n-k]*d[k]%mod, mod-2)%mod <<"\n";
+    }
+}
