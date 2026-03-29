e1e4d462061495e3fe2fa2d37255d83b27ad35bb
wanjun kim <71976009+kst54252@users.noreply.github.com>
2026-03-29 23:00:40 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 0 ms, Memory: 2024 KB, Score: 62 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.cc
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.cc
@@ -10,7 +10,7 @@ int main() {
     cin >> s;
     int cnt = 0, num = 0;
     for(int i=0;i<n;i++){
-        if(num*10+s[i]-'0' > 641){
+        if(num*10+s[i]-'0' > 641 || (s[i+1] == '0' && s[i+2] == '0')){
             cnt++;
             num = s[i]-'0';
         }
